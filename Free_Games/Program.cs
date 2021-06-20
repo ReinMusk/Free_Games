cd115c24a4ec1ba8ba8af905f25f2474ba851c0d
ReinMusk <dragon0lordchik@gmail.com>
2021-06-20 18:56:22 +0300

    Now when you enter any value in the bot chat, a random game is displayed

--- Free_Games/Program.cs
+++ Free_Games/Program.cs
@@ -5,26 +5,60 @@ using System.Collections.Generic;
 using System.IO;
 using System.Net;
 using Newtonsoft.Json;
+using Telegram.Bot;
 
 namespace Free_Games
 {
     class Program
     {
-        static void Main()
+        [Obsolete]
+        static void Main(string[] args)
+        {
+            TelegramBotClient bot = new TelegramBotClient("1814776711:AAFjfToliQ7UJ_CVHvxoawW_cNKxGc98NI4");
+            //var bot = new TelegramBotClient(File.ReadAllText("C:\\Work\\token.txt"));
+
+            bot.OnMessage += (s, arg) =>
+            {
+                var game = GetGame();
+
+                Random rnd = new Random();
+                int value = rnd.Next(0, game.Count);
+
+                Console.WriteLine(game[value].id.ToString());
+                Console.WriteLine(game[value].title);
+                Console.WriteLine(game[value].genre);
+                Console.WriteLine(game[value].developer);
+                Console.WriteLine(game[value].thumbnail);
+                Console.WriteLine(game[value].short_description);
+                Console.WriteLine(game[value].game_url);
+
+                bot.SendTextMessageAsync(arg.Message.Chat.Id, game[value].id.ToString());
+                bot.SendTextMessageAsync(arg.Message.Chat.Id, game[value].title);
+                bot.SendTextMessageAsync(arg.Message.Chat.Id, game[value].genre);
+                bot.SendTextMessageAsync(arg.Message.Chat.Id, game[value].developer);
+                bot.SendTextMessageAsync(arg.Message.Chat.Id, game[value].thumbnail);
+                bot.SendTextMessageAsync(arg.Message.Chat.Id, game[value].short_description);
+                bot.SendTextMessageAsync(arg.Message.Chat.Id, game[value].game_url);  
+            };
+
+            bot.StartReceiving();
+
+            Console.ReadKey();
+        }
+
+        public static List<Free_Games.Root> GetGame()
         {
             var platform = "pc";
 
             var url = $"https://www.freetogame.com/api/games?platform={platform}";
 
             var request = WebRequest.Create(url);
-
             var response = request.GetResponse();
             var httpStatusCode = (response as HttpWebResponse).StatusCode;
 
             if (httpStatusCode != HttpStatusCode.OK)
             {
                 Console.WriteLine(httpStatusCode);
-                return;
             }
 
             using (var streamReader = new StreamReader(response.GetResponseStream()))
@@ -36,31 +70,22 @@ namespace Free_Games
                 Random rnd = new Random();
                 int value = rnd.Next(0, myDeserializedClass.Count);
 
-
-                Console.WriteLine(myDeserializedClass[value].id);
-                Console.WriteLine(myDeserializedClass[value].title);
-                Console.WriteLine(myDeserializedClass[value].thumbnail);
-                Console.WriteLine(myDeserializedClass[value].short_description);
-                Console.WriteLine(myDeserializedClass[value].game_url);
-                Console.WriteLine(myDeserializedClass[value].genre);
-                Console.WriteLine(myDeserializedClass[value].developer);
+                return myDeserializedClass;
             }
-
         }
-
-        public class Root
-        {
-            public int id { get; set; }
-            public string title { get; set; }
-            public string thumbnail { get; set; }
-            public string short_description { get; set; }
-            public string game_url { get; set; }
-            public string genre { get; set; }
-            public string platform { get; set; }
-            public string publisher { get; set; }
-            public string developer { get; set; }
-            public string release_date { get; set; }
-            public string freetogame_profile_url { get; set; }
-        }
+    }
+    public class Root
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public string thumbnail { get; set; }
+        public string short_description { get; set; }
+        public string game_url { get; set; }
+        public string genre { get; set; }
+        public string platform { get; set; }
+        public string publisher { get; set; }
+        public string developer { get; set; }
+        public string release_date { get; set; }
+        public string freetogame_profile_url { get; set; }
     }
 }
---
+++ Games_Bot/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using Telegram.Bot;
+using Newtonsoft.Json;
+using Free_Games;
+
+namespace TelegramBot
+{
+    class Program
+    {
+        [Obsolete]
+        static void Main(string[] args)
+        {
+            
+
+            TelegramBotClient bot = new TelegramBotClient("1814776711:AAFjfToliQ7UJ_CVHvxoawW_cNKxGc98NI4");
+            //var bot = new TelegramBotClient(File.ReadAllText("C:\\Work\\token.txt"));
+
+            bot.OnMessage += (s, arg) =>
+            {
+                Console.WriteLine($"{arg.Message.Chat.FirstName}: {arg.Message.Text}");
+                bot.SendTextMessageAsync(arg.Message.Chat.Id, $"You say: {arg.Message.Text}");
+            };
+
+            bot.StartReceiving();
+
+            Console.ReadKey();
+        }
+    }
+}
